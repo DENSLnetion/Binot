8ea1947703bc4f5d7b2eebf2a9f72830eb33e274
DENSLnetion <lyoncintakeju@gmail.com>
2026-06-19 06:03:26 +0700

    Add synchronous methods to NoteRepository

--- app/src/main/java/com/example/data/NoteRepository.kt
+++ app/src/main/java/com/example/data/NoteRepository.kt
@@ -3,8 +3,13 @@ package com.example.data
 class NoteRepository(private val noteDao: NoteDao) {
     val allNotes = noteDao.getAllNotes()
 
+    suspend fun getAllNotesSync() = noteDao.getAllNotesSync()
+    suspend fun deleteAllNotes() = noteDao.deleteAllNotes()
+    suspend fun insertNotes(notes: List<NoteEntity>) = noteDao.insertNotes(notes)
+
     suspend fun insert(note: NoteEntity) = noteDao.insertNote(note)
     suspend fun update(note: NoteEntity) = noteDao.updateNote(note)
     suspend fun deleteById(id: Int) = noteDao.deleteNoteById(id)
     suspend fun getNoteById(id: Int) = noteDao.getNoteById(id)
 }
+
