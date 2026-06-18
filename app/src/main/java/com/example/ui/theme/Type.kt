3b66c0e67fe88ec6c4266bacde3ef058fd94c0a2
DENSLnetion <lyoncintakeju@gmail.com>
2026-06-19 03:51:08 +0700

    Change font family to GoogleSansFlex in Typography
    Updated typography to use GoogleSansFlex font family.

--- app/src/main/java/com/example/ui/theme/Type.kt
+++ app/src/main/java/com/example/ui/theme/Type.kt
@@ -2,41 +2,51 @@ package com.example.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.R
+
+// PASTIKAN LU UDAH MASUKIN FILE google_sans_flex.ttf KE FOLDER res/font/
+val GoogleSansFlex = FontFamily(
+    Font(R.font.google_sans_flex, FontWeight.Normal),
+    Font(R.font.google_sans_flex, FontWeight.Medium),
+    Font(R.font.google_sans_flex, FontWeight.Bold),
+    Font(R.font.google_sans_flex, FontWeight.ExtraBold)
+)
 
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = GoogleSansFlex,
         fontWeight = FontWeight.Black,
         fontSize = 64.sp,
         lineHeight = 72.sp,
         letterSpacing = (-1.5).sp,
     ),
     headlineMedium = TextStyle(
-        fontFamily = FontFamily.SansSerif,
+        fontFamily = GoogleSansFlex,
         fontWeight = FontWeight.ExtraBold,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = (-0.5).sp
     ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
+        fontFamily = GoogleSansFlex,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = (-0.2).sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
+        fontFamily = GoogleSansFlex,
         fontWeight = FontWeight.Medium,
         fontSize = 18.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp,
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily.SansSerif,
+        fontFamily = GoogleSansFlex,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
