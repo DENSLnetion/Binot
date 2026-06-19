a93b0ea14bf1631f211d6534e07b6a145d475e9f
DENSLnetion <lyoncintakeju@gmail.com>
2026-06-20 00:43:12 +0700

    Update database version to 3
    Increment database version to 3 to accommodate new column.

--- app/src/main/java/com/example/data/AppDatabase.kt
+++ app/src/main/java/com/example/data/AppDatabase.kt
@@ -3,8 +3,8 @@ package com.example.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-// Versi database naik jadi 2 karena ada tambahan kolom isPinned
-@Database(entities = [NoteEntity::class], version = 2, exportSchema = false)
+@Database(entities = [NoteEntity::class], version = 3, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
+
