3e1a885e66641cb7f19b0d5ea753cd64820cefa6
DENSLnetion <lyoncintakeju@gmail.com>
2026-06-21 02:22:30 +0700

    Remove commented notes from NoteRepository

--- app/src/main/java/com/example/data/NoteRepository.kt
+++ app/src/main/java/com/example/data/NoteRepository.kt
@@ -2,16 +2,16 @@ package com.example.data
 
 class NoteRepository(private val noteDao: NoteDao) {
     val allNotes = noteDao.getAllNotes()
-    val trashedNotes = noteDao.getTrashedNotes() // Flow buat layar Trash
+    val trashedNotes = noteDao.getTrashedNotes()
 
     suspend fun getAllNotesSync() = noteDao.getAllNotesSync()
     suspend fun deleteAllNotes() = noteDao.deleteAllNotes()
-    suspend fun emptyTrash() = noteDao.emptyTrash() // Kosongin sampah
+    suspend fun emptyTrash() = noteDao.emptyTrash()
     suspend fun insertNotes(notes: List<NoteEntity>) = noteDao.insertNotes(notes)
 
     suspend fun insert(note: NoteEntity) = noteDao.insertNote(note)
     suspend fun update(note: NoteEntity) = noteDao.updateNote(note)
-    suspend fun deleteById(id: Int) = noteDao.deleteNoteById(id) // Delete Permanen
+    suspend fun deleteById(id: Int) = noteDao.deleteNoteById(id)
     suspend fun getNoteById(id: Int) = noteDao.getNoteById(id)
 }
 
