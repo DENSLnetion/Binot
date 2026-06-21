9f188d66745f796c9aae4556d9dfee03a17e1c29
DENSLnetion <lyoncintakeju@gmail.com>
2026-06-21 16:10:36 +0700

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,9 +14,8 @@ android {
     applicationId = "com.aistudio.binot.xrztp"
     minSdk = 24
     targetSdk = 36
-    // NAIKIN JADI VERSI 1.0.0
-    versionCode = 2 
-    versionName = "1.0.0"
+    versionCode = 3 
+    versionName = "1.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
