dfd8cefa939f56eb7c9d0435006ba1c893663488
DENSLnetion <lyoncintakeju@gmail.com>
2026-06-22 00:19:38 +0700

    Bump version code to 5 and version name to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.binot.xrztp"
     minSdk = 24
     targetSdk = 36
-    versionCode = 4 
-    versionName = "1.0.2"
+    versionCode = 5 
+    versionName = "1.0.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
