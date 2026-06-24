5415740487daea35037f3e67b922f81460b196a7
DENSLnetion <lyoncintakeju@gmail.com>
2026-06-25 03:09:14 +0700

    Bump version code to 9 and version name to 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.binot.xrztp"
     minSdk = 24
     targetSdk = 36
-    versionCode = 8 
-    versionName = "1.0.6"
+    versionCode = 9 
+    versionName = "1.0.7"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
